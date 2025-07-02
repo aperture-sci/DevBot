5c24cda9d28d4187e59386d37146c2086206e55c
lrochette <laurent.rochette@codefresh.io>
2025-07-02 13:59:37 -0700

    0.0.20 simplyfing
    Signed-off-by: lrochette <laurent.rochette@codefresh.io>

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,3 @@
-APP_SERVICES_LIST = ["ctrlr", "flask-ui", "buslog"]
-
 DEVBOT_TASKS = ['dev', 'merge_prs', 'close_prs', 'clean_changes']
 GH_API_BASE_URL = "https://api.github.com"
 JSON_DATA_FILE = "devbot_params.json"
